b6e952fc9508531e823020e2e6193477a26d8a51
Hendrik Bergunde <hbergunde@gmx.de>
2020-06-08 23:00:50 +0200

    Add hand+suit logic

--- main.rb
+++ main.rb
@@ -35,45 +35,111 @@ end
 
 
 class PokerHand
+  attr_reader :my_hand
   def initialize(my_hand_string)
     @my_hand = representation_of(my_hand_string)
   end
 
-  def high_sequence_of(number_of_cards, of_suite = nil)
-    byebug
+  ## Helper methods identifying special suits ##
+
+  def has_flush
+    my_hand.group_by(&:suit).select{|_,hand_of_suit|hand_of_suit.count ==5}
   end
 
-  def is_royalflush?(hand)
-    highest_cards = hand.select {|card| card.value >= TEN}
+  def has_straight
+    sequence = [my_hand.first]
+    my_hand.each do |card|
+      next if card == sequence.first
+      if sequence.last.value == card.value+1
+        sequence << card
+      else
+        sequence = [card]
+      end
+    end
+    return [] unless sequence.count == 5
+    sequence
+  end
+
+  def self.has_of_a_kind(hand,number)
+    hand.group_by(&:value).select{|_,many_of_a_kind| many_of_a_kind.count == number}.values
+  end
+  def has_of_a_kind(number)
+    PokerHand.has_of_a_kind(my_hand,number)
+  end
+
+  ## booleans figuring out each named suit
+
+  def is_royalflush?
+    highest_cards = my_hand.select {|card| card.value >= PokerCard::TEN}
     highest_cards.count == 5 && highest_cards.map(&:suit).uniq.count == 1
   end
-  def is_straightflush?(hand)
-    PokerHand.with_high_sequence_of(5).any?
+  def is_straightflush?
+    has_straight.map(&:suit).uniq.count == 1
+  end
+  def is_fourofakind?
+    not(has_of_a_kind(4).empty?)
+  end
+  def has_threeofakind
+    has_of_a_kind(3).flatten
+  end
+  def has_fullhouse
+    threeofakind = has_threeofakind
+    remaining_cards = my_hand - threeofakind
+    return my_hand if remaining_cards.map(&:value).uniq.count == 1
+    []
+  end
+  def is_fullhouse?
+    has_fullhouse.any?
+  end
+  def is_flush?
+    has_flush.any?
+  end
+  def is_straight?
+    has_straight.any?
+  end
+  def is_threeofakind?
+    has_threeofakind.any?
+  end
+  def has_twopairs
+    if is_pair?
+      higher_pair = has_pair
+      lower_pair = PokerHand.has_of_a_kind(my_hand - higher_pair,2).flatten
+      return higher_pair + lower_pair if lower_pair.any?
+    end
+    []
+  end
+  def is_twopairs?
+    has_twopairs.any?
+  end
+  def has_pair
+    pair = has_of_a_kind(2)
+    return pair.first unless pair.empty?
+    []
+  end
+  def is_pair?
+    has_pair.any?
+  end
 
-  end
-  def is_fourofakind?(hand)
-    false
-  end
-  def is_fullhouse?(hand)
-    false
-  end
-  def is_flush?(hand)
-    hand.group_by(&:suit).select{|_,hand_of_suit|hand_of_suit.count ==5}.any?
-  end
-  def is_straight?(hand)
-    PokerHand.with_high_sequence_of(5).any?
-    false
-  end
-  def is_threeofakind?(hand)
-    false
-  end
-  def is_twopairs?(hand)
-    false
-  end
-  def is_pair?(hand)
-    false
+  def identify_suit
+    return :royalflush if is_royalflush?
+    return :straightflush if is_straightflush?
+    return :fourofakind if is_fourofakind?
+    return :fullhouse if is_fullhouse?
+    return :flush if is_flush?
+    return :straight if is_straight?
+    return :threeofakind if is_threeofakind?
+    return :twopairs if is_twopairs?
+    return :pair if is_pair?
+    return :highcard
   end
 
+  def compare_with(other_hand_string)
+    other_hand = PokerHand.new(other_hand_string)
+    return "Tie" if other_hand == @my_hand
+    return identify_suit
+  end
+
+  private
   def representation_of(hand_string)
     hand_string.split(" ").map do |cardcode|
       begin
@@ -83,31 +149,12 @@ class PokerHand
       rescue => e
         raise "Did not recognize #{hand_string} card: #{e.message}"
       end
-    end.sort_by {|card| card.value}
-  end
-
-  def identify_suite(hand)
-    return :royalflush if is_royalflush?(hand)
-    return :straightflush if is_straightflush?(hand)
-    return :fourofakind if is_fourofakind?(hand)
-    return :fullhouse if is_fullhouse?(hand)
-    return :flush if is_flush?(hand)
-    return :straight if is_straight?(hand)
-    return :threeofakind if is_threeofakind?(hand)
-    return :twopairs if is_twopairs?(hand)
-    return :pair if is_pair?(hand)
-    return :highcard
-  end
-
-  def compare_with(other_hand_string)
-    other_hand = representation_of(other_hand_string)
-    return "Tie" if other_hand == @my_hand
-    return identify_suite(@my_hand)
+    end.sort_by {|card| card.value}.reverse
   end
 end
 
 # HAND = "KS 2H 5C JD TD"
-HAND = "9H TH JH QH KH"
+HAND = "KH KS 9C 9D 9H"
 
 ph = PokerHand.new(HAND)
 puts ph.compare_with("KS 2H 5C JD TD")
